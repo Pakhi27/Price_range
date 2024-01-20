99673e25799ebf5c2856db7546d50f0f87cc5c81
Pakhi27 <137711794+Pakhi27@users.noreply.github.com>
2024-01-20 22:14:38 +0530

    Add files via upload

---
+++ price_range.js
@@ -0,0 +1,11 @@
+function updateMinPrice(value) {
+    document.querySelector('.b1').value = value;
+}
+
+function updateMaxPrice(value) {
+    document.querySelector('.b2').value = value;
+}
+document.getElementsByClassName("heading")[0].style.color="green"
+document.getElementsByClassName("sub-heading")[0].style.color="green"
+document.getElementsByClassName("b1")[0].style.color="green"
+document.getElementsByClassName("b2")[0].style.color="green"
